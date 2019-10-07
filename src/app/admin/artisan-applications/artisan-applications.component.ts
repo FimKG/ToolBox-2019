c31f04f913540c40fba9d236b881eb741244d346
Percy Mohale <percymohale9@gmail.com>
2019-10-07 07:32:24 -0700

    cleaned Admin

--- src/app/admin/artisan-applications/artisan-applications.component.ts
+++ src/app/admin/artisan-applications/artisan-applications.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AdminServiceService} from '../../admin-service.service';
 
 @Component({
   selector: 'app-artisan-applications',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtisanApplicationsComponent implements OnInit {
 
-  constructor() { }
+  applications: any;
+  categories: any;
+  constructor(private adminservice: AdminServiceService) { }
 
   ngOnInit() {
+    this.GetAllApplications();
+    this. GetAllCategories();
+  }
+
+  GetAllApplications()
+  {
+    this.adminservice.getAllArtisanApplication().subscribe(data=>{
+      this.applications = data.data;
+      console.log(this.applications);
+    })
+  }
+
+  GetAllCategories()
+  {
+    this.adminservice.GetAllCategories().subscribe(data=>{
+      this.categories = data.data;
+      console.log(data.data);
+    })
   }
 
 }
