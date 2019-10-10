89e65183185edccefa423b0b1c1a35778db87798
calvin mogale <mogalecalvin@gmail.com>
2019-10-10 16:08:44 +0200

    modal on artisans

--- src/app/admin/artisans/artisans.component.ts
+++ src/app/admin/artisans/artisans.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {AdminServiceService} from '../../admin-service.service';
+import { Component, OnInit, Input } from '@angular/core';
+import { AdminServiceService } from '../../admin-service.service';
 
 @Component({
   selector: 'app-artisans',
@@ -7,48 +7,57 @@ import {AdminServiceService} from '../../admin-service.service';
   styleUrls: ['./artisans.component.css']
 })
 export class ArtisansComponent implements OnInit {
-artisans:any;
-categories:any;
-selectedCategory: number;
+  artisans: any;
+  categories: any;
+  // artis: any;
+  
+  selectedCategory: number;
+
+  @Input() artis = {
+    name: '',
+    surname: '',
+    description: ''
+  }
 
   constructor(private adminservice: AdminServiceService) { }
 
   ngOnInit() {
-    this. getAllArtisans();
+    this.getAllArtisans();
     this.getCategories();
   }
 
-  getAllArtisans()
-  {
-    this.adminservice.getAllArtisanApplication().subscribe(data=>{
+  getAllArtisans() {
+    this.adminservice.getAllArtisanApplication().subscribe(data => {
       this.artisans = data.data;
       console.log(data.data);
     })
   }
 
-  getCategories()
-  {
+  getCategories() {
 
-    this.adminservice.GetAllCategories().subscribe(resp =>{
-        this.categories = resp.data;
-        console.log(this.categories);
+    this.adminservice.GetAllCategories().subscribe(resp => {
+      this.categories = resp.data;
+      console.log(this.categories);
     })
   }
 
-  getSelectedCat(category)
-  {
+  getSelectedCat(category) {
     this.selectedCategory = category.catID;
     console.log(this.selectedCategory);
-    this. getArtisansBasedOnCategory(this.selectedCategory);
-   
+    this.getArtisansBasedOnCategory(this.selectedCategory);
+
   }
 
-  getArtisansBasedOnCategory(id)
-  {
-    this.adminservice.GetArtsansbasedOnCatgory(id).subscribe(data=>{
+  getArtisansBasedOnCategory(id) {
+    this.adminservice.GetArtsansbasedOnCatgory(id).subscribe(data => {
       this.artisans = data.data;
       console.log(this.artisans);
     })
+  }
+
+  GetEachArtisan(art) {
+    this.artis = art;
+    console.log(art);
   }
 
 }
